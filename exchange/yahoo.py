d54cfda50a52745fd57e643cb009c668ce0f7d70
Sergey Grebenyuk <segrebenyuk@sberbank.ru>
2022-05-21 18:57:25 +0300

    load hourly

--- exchange/yahoo.py
+++ exchange/yahoo.py
@@ -9,7 +9,7 @@ def load(ticker= 'MSFT', startTime='2022-01-01'):
         # period : str
             # Valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
             # Either Use period parameter or use start and end
-        interval='1mo',
+        interval='1h',
             # Valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
             # Intraday data cannot extend last 60 days
         start= startTime,
