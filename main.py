5bbdb89fb536176c3a079bf655db24ba00e847da
Sergey Grebenyuk <segrebenyuk@sberbank.ru>
2022-06-08 00:55:55 +0300

    gap expert

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from files.get_tickers import load_tickers
 from service.firstlaunch import first_launch
 
 from service.marketdata import update_market_info
-from service.signals import update_signals, publish_alerts
+from service.signals import publish_alerts, update_signals_fibo, update_signals_gap
 
 # schedule.every().hour.do(update_market_info)
 # schedule.every().hour.do(update_signals)
@@ -16,11 +16,12 @@ from service.signals import update_signals, publish_alerts
 
 
 
-# first_launch()
+first_launch()
 print('start update market info')
-# update_market_info()
+update_market_info()
 print('start update signals')
-# update_signals()
+update_signals_fibo()
+update_signals_gap()
 print('start sending alerts')
 publish_alerts()
 print('finish mf')
